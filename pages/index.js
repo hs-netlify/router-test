5746cdc2238da5ac9297c7bf5edbf8252c66d979
Henry Smith <henry.smith@netlify.com>
2023-06-07 17:29:08 +0100

    Remove other buttons

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,6 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Link href="/gb/en/db">A Simple button to the DB (GB-EN)</Link>
-      <br />
-      <Link href="/us/en/db">A Simple button to the DB (US-EN)</Link>
-      <br />
-      <Link href="/fr/fr/db">A Simple button to the DB (FR-FR)</Link>
     </div>
   );
 }
